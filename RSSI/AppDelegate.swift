632f35d5b50729662331e10533afb84532af9ee8
TomMJG <912135959@qq.com>
2015-11-17 23:15:34 +0800

    Initial Commit

---
+++ RSSI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RSSI
+//
+//  Created by 马家固 on 15/11/17.
+//  Copyright © 2015年 马家固. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RSSI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RSSI
+//
+//  Created by 马家固 on 15/11/17.
+//  Copyright © 2015年 马家固. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
